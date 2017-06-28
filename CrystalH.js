400913d4a90e6b50fe2f7c7536df88e063ca796b
cnhoward <crissyh27@gmail.com>
2017-06-28 17:14:53 -0400

    add listeners

---
+++ CrystalH.js
@@ -0,0 +1,24 @@
+console.log('This works better now')
+var cookies = [
+  "chocolate chip",
+  "the lemon ones",
+  "oreos",
+  "samoa",
+  "oatmeal",
+];
+function eat(cookies) {
+console.log('I just ate ' + cookies);
+}
+function eatCookies(){
+for (var i = 0; i < cookies.length; i = i + 1) {
+  eat(cookies[i]);
+}}
+
+var eatButton = $(".devour");
+eatButton.on("click", eatCookies);
+var text = $(".myClass");
+
+var colorButton = $(".color")
+colorButton.on("click", function() {
+  text.toggleClass("green")
+});
